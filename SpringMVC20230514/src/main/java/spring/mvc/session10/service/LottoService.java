878c2b8b83a118b80d513545de08edc0fa227b6a
vincenttuan <vincentjava@yahoo.com.tw>
2023-05-28 13:47:42 +0800

    lotto

---
+++ SpringMVC20230514/src/main/java/spring/mvc/session10/service/LottoService.java
@@ -0,0 +1,41 @@
+package spring.mvc.session10.service;
+
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LottoService {
+	
+	// 用來存放 Lotto 紀錄的集合
+	private List<Set<Integer>> lottos = new CopyOnWriteArrayList<>(); // 支援多執行緒的集合(Thread-safe)
+	
+	// 新增一筆紀錄
+	
+	// 查詢多筆紀錄
+	
+	// 修改指定紀錄(一整列)
+	
+	// 修改指定紀錄(某一個欄位)
+	
+	// 刪除指定紀錄(一整列)
+	
+	// 透過電腦選號產生號碼
+	private Set<Integer> getRandomLotto() {
+		// 樂透 539: 1~39 取出任意不重複的五個數字
+		Set<Integer> lotto = new LinkedHashSet<>();
+		Random random = new Random();
+		while (lotto.size() < 5) {
+			int number = random.nextInt(39) + 1; // 1~39
+			lotto.add(number);
+		}
+		return lotto;
+	}
+	
+	// 分組統計資料與排序
+	
+}
