55927f734eb00b31642ffb853cf58817a3cf9e5a
vincenttuan <vincentjava@yahoo.com.tw>
2023-05-28 15:16:52 +0800

    mvc

--- SpringMVC20230514/src/main/java/spring/mvc/session10/controller/LottoController.java
+++ SpringMVC20230514/src/main/java/spring/mvc/session10/controller/LottoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -34,6 +35,20 @@ public class LottoController {
 		Set<Integer> lotto = lottoService.add();
 		attr.addAttribute("lotto", lotto); // 放在網址列？參數中
 		//attr.addFlashAttribute("lotto", lotto); // 放在暫存的/一次性的 session 屬性中
+		return "redirect:./"; // 重導至 "/" 首頁
+	}
+	
+	// 修改 lotto
+	@GetMapping("/update/{index}")
+	public String update(@PathVariable("index") int index) {
+		lottoService.update(index);
+		return "redirect:./"; // 重導至 "/" 首頁
+	}
+	
+	// 刪除 lotto
+	@GetMapping("/delete/{index}")
+	public String delete(@PathVariable("index") int index) {
+		lottoService.delete(index);
 		return "redirect:./"; // 重導至 "/" 首頁
 	}
 }
