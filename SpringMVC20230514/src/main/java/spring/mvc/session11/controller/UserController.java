946539f1170de5e779d8d0837cb69f7e5170b8de
vincenttuan <vincentjava@yahoo.com.tw>
2023-06-04 15:34:54 +0800

    mvc

--- SpringMVC20230514/src/main/java/spring/mvc/session11/controller/UserController.java
+++ SpringMVC20230514/src/main/java/spring/mvc/session11/controller/UserController.java
@@ -5,10 +5,12 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -71,5 +73,18 @@ public class UserController {
 		return "redirect:./";
 	}
 	
+	// 修改
+	@PutMapping("/{index}")
+	public String update(@PathVariable("index") int index, @ModelAttribute User user) {
+		users.set(index, user);
+		return "redirect:./";
+	}
+	
+	// 刪除
+	@DeleteMapping("/{index")
+	public String delete(@PathVariable("index") int index) {
+		users.remove(index);
+		return "redirect:./";
+	}
 	
 }
