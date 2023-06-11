4b7b47aef63524030fce187355b82d8182a10367
vincenttuan <vincentjava@yahoo.com.tw>
2023-06-11 14:19:48 +0800

    mvc

--- SpringMVC20230514/src/main/java/spring/mvc/session13/entity/Person.java
+++ SpringMVC20230514/src/main/java/spring/mvc/session13/entity/Person.java
@@ -14,19 +14,19 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class Person {
 	
-	@NotEmpty(message = "姓名不可以是空值")
-	@Size(min = 2, max = 50, message = "姓名必須介於{min}~{max}個字之間")
+	@NotEmpty(message = "{person.name.empty}")
+	@Size(min = 2, max = 50, message = "{person.name.size}")
 	private String name; // 姓名
 	
-	@NotNull(message = "年齡不可以是空值")
-	@Range(min = 18, max = 99, message = "年齡必須介於{min}~{max}歲之間")
+	@NotNull(message = "{person.age.null}")
+	@Range(min = 18, max = 99, message = "{person.age.range}")
 	private Integer age; // 年齡
 	
-	@NotNull(message = "會員設定不可以是空值")
+	@NotNull(message = "{person.member.null}")
 	private Boolean member; // 是否是會員
 	
-	@NotNull(message = "生日日期設定不可以是空值")
-	@Past(message = "生日不可以大於現在日期")
+	@NotNull(message = "{person.birth.null}")
+	@Past(message = "{person.birth.past}")
 	@DateTimeFormat(pattern = "yyyy-MM-dd") // 將網頁表單傳來的日期格式轉成 Date 物件
 	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8") // Date 物件轉日期字串(當前端欲收到 json 字串的時候)
 	private Date birth; // 生日
