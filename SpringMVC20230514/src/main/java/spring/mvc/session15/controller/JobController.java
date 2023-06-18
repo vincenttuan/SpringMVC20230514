2d2c430337a61f3f087806a4e7bf963c974c9a85
vincenttuan <vincentjava@yahoo.com.tw>
2023-06-18 16:33:52 +0800

    jdbc

--- SpringMVC20230514/src/main/java/spring/mvc/session15/controller/JobController.java
+++ SpringMVC20230514/src/main/java/spring/mvc/session15/controller/JobController.java
@@ -1,13 +1,17 @@
 package spring.mvc.session15.controller;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import javax.validation.constraints.Past;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import spring.mvc.session15.entity.Job;
@@ -49,6 +53,20 @@ public class JobController {
 		model.addAttribute("job", jobDao.get(jid));
 		setBaseModelAttribute(model, session);
 		return "session15/job";
+	}
+	
+	@PostMapping("/")
+	public String add(@ModelAttribute @Valid Job job, BindingResult result, Model model, HttpSession session) {
+		if(result.hasErrors()) {
+			model.addAttribute("_method", "POST");
+			setBaseModelAttribute(model, session);
+			return "session15/job";
+		}
+		// 新增資料
+		jobDao.add(job);
+		
+		return "redirect:./";
+		
 	}
 	
 	// 取得總頁數
