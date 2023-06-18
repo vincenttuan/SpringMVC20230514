3abe9bdbe4aa3b2fa85e52652c7aab0d573a47b1
vincenttuan <vincentjava@yahoo.com.tw>
2023-06-18 16:08:56 +0800

    jdbc

--- SpringMVC20230514/src/main/java/spring/mvc/session15/controller/JobController.java
+++ SpringMVC20230514/src/main/java/spring/mvc/session15/controller/JobController.java
@@ -1,5 +1,7 @@
 package spring.mvc.session15.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,6 +24,33 @@ public class JobController {
 	@Autowired
 	private EmployeeDao employeeDao;
 	
+	@GetMapping("/")
+	public String index(@ModelAttribute Job job, Model model, HttpSession session)  {
+		model.addAttribute("_method", "POST");
+		setBaseModelAttribute(model, session);
+		return "session15/job";
+	}
+	
+	@GetMapping("/page/{pageNo}")
+	public String page(@PathVariable("pageNo") int pageNo, @ModelAttribute Job job, Model model, HttpSession session) {
+		if(pageNo < 0) {
+			session.setAttribute("num", ""); // 將 session 內容清空
+			return "redirect:../";
+		}
+		session.setAttribute("num", pageNo); // 存入目前使用者所在 page
+		model.addAttribute("_method", "POST");
+		setBaseModelAttribute(model, session);
+		return "session15/job";
+	}
+	
+	@GetMapping("/{jid}")
+	public String get(@PathVariable("jid") Integer jid, Model model, HttpSession session) {
+		model.addAttribute("_method", "PUT");
+		model.addAttribute("job", jobDao.get(jid));
+		setBaseModelAttribute(model, session);
+		return "session15/job";
+	}
+	
 	// 取得總頁數
 	private int getPageCount() {
 		int count = jobDao.getCount();
@@ -30,39 +59,17 @@ public class JobController {
 		return pageCount;
 	}
 	
-	@GetMapping("/")
-	public String index(@ModelAttribute Job job, Model model)  {
-		model.addAttribute("_method", "POST");
-		
-		model.addAttribute("employees", employeeDao.query());
-		model.addAttribute("jobs", jobDao.query());
-		model.addAttribute("pageCount", getPageCount());
-		return "session15/job";
-	}
-	
-	@GetMapping("/page/{pageNo}")
-	public String page(@PathVariable("pageNo") int pageNo, @ModelAttribute Job job, Model model) {
-		if(pageNo < 0) {
-			return "redirect:../";
+	// 設定 base model attr value
+	private void setBaseModelAttribute(Model model, HttpSession session) {
+		// 取得目前使用者所在的分頁
+		String sessionNum = session.getAttribute("num") + "";
+		if(sessionNum.length() > 0 && !sessionNum.equals("null")) {
+			int num = Integer.parseInt(sessionNum);
+			model.addAttribute("jobs", jobDao.queryPage(num));
+		} else {
+			model.addAttribute("jobs", jobDao.query());
 		}
-		model.addAttribute("_method", "POST");
-		
 		model.addAttribute("employees", employeeDao.query());
-		model.addAttribute("jobs", jobDao.queryPage(pageNo));
 		model.addAttribute("pageCount", getPageCount());
-		return "session15/job";
 	}
-	
-	@GetMapping("/{jid}")
-	public String get(@PathVariable("jid") Integer jid, Model model) {
-		model.addAttribute("_method", "PUT");
-		model.addAttribute("job", jobDao.get(jid));
-		
-		model.addAttribute("employees", employeeDao.query());
-		model.addAttribute("jobs", jobDao.query());
-		model.addAttribute("pageCount", getPageCount());
-		return "session15/job";
-	}
-	
-	
 }
