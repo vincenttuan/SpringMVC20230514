a00c201de940e79a913e7ce3379cfa6a220a4f4a
vincenttuan <vincentjava@yahoo.com.tw>
2023-06-18 14:12:42 +0800

    jdbc

---
+++ SpringMVC20230514/src/main/java/spring/mvc/session15/repository/JobDaoImpl.java
@@ -0,0 +1,60 @@
+package spring.mvc.session15.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import spring.mvc.session15.entity.Job;
+
+@Repository
+public class JobDaoImpl implements JobDao {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Override
+	public int add(Job job) {
+		String sql = SQLUtil.ADD_JOB_SQL;
+		return jdbcTemplate.update(sql, job.getJname(), job.getEid());
+	}
+
+	@Override
+	public int update(Job job) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Integer jid) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Job get(Integer jid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Job> query() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Job> queryPage(int pageNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
