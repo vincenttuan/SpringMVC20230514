ddc14250abe8831bec18d661e42d205cbd7d02d2
vincenttuan <vincentjava@yahoo.com.tw>
2023-06-18 14:03:26 +0800

    jdbc

--- SpringMVC20230514/src/main/java/spring/mvc/session15/repository/SQLUtil.java
+++ SpringMVC20230514/src/main/java/spring/mvc/session15/repository/SQLUtil.java
@@ -3,7 +3,7 @@ package spring.mvc.session15.repository;
 public class SQLUtil {
 	// employee table
 	public static final String ADD_EMPLOYEE_SQL = "insert into employee (ename, salary) values(?, ?)"; 
-	public static final String UPT_EMPLOYEE_SQL = "update employee set ename=?, salary=? where eid=?";; 
+	public static final String UPT_EMPLOYEE_SQL = "update employee set ename=?, salary=? where eid=?"; 
 	public static final String DEL_EMPLOYEE_SQL = "delete from employee where eid=?"; 
 	public static final String GET_EMPLOYEE_SQL = "select eid, ename, salary, createtime from employee where eid=?"; 
 	public static final String COUNT_EMPLOYEE_SQL = "select count(*) from employee"; 
