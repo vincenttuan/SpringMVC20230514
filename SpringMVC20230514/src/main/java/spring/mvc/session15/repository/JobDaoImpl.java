15cc97c7d3310f87268a77a881adee4cc3e94549
vincenttuan <vincentjava@yahoo.com.tw>
2023-06-18 15:30:51 +0800

    jdbc

--- SpringMVC20230514/src/main/java/spring/mvc/session15/repository/JobDaoImpl.java
+++ SpringMVC20230514/src/main/java/spring/mvc/session15/repository/JobDaoImpl.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import spring.mvc.session15.entity.Employee;
 import spring.mvc.session15.entity.Job;
 
 @Repository
@@ -14,7 +16,10 @@ public class JobDaoImpl implements JobDao {
 	
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-
+	
+	@Autowired
+	private EmployeeDao employeeDao;
+	
 	@Override
 	public int add(Job job) {
 		String sql = SQLUtil.ADD_JOB_SQL;
@@ -48,7 +53,19 @@ public class JobDaoImpl implements JobDao {
 	@Override
 	public List<Job> query() {
 		String sql = SQLUtil.QUERY_JOB_SQL; // 不分頁查詢
-		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<Job>(Job.class));
+		// 自行將每一筆紀錄的 Job 組合 Employee
+		RowMapper<Job> rm = (rs, rowNum) -> {
+			Job job = new Job();
+			job.setJid(rs.getInt("jid"));
+			job.setJname(rs.getString("jname"));
+			job.setEid(rs.getInt("eid"));
+			// job 的 eid 透過 employeeDao 來取得 employee 物件
+			Employee emp = employeeDao.get(job.getEid());
+			// 組合 Employee
+			job.setEmployee(emp);
+			return job;
+		};
+		return jdbcTemplate.query(sql, rm);
 	}
 
 	@Override
