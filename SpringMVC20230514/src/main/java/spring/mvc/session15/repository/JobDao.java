361a25398afb59ae79af754789d97dfe2d90e847
vincenttuan <vincentjava@yahoo.com.tw>
2023-06-18 13:55:51 +0800

    jdbc

---
+++ SpringMVC20230514/src/main/java/spring/mvc/session15/repository/JobDao.java
@@ -0,0 +1,31 @@
+package spring.mvc.session15.repository;
+
+import java.util.List;
+
+import spring.mvc.session15.entity.Job;
+
+public interface JobDao {
+	// 每頁筆數
+	int LIMIT = 5;
+	
+	// 新增單筆
+	int add(Job job);
+	
+	// 修改單筆
+	int update(Job job);
+	
+	// 刪除單筆
+	int delete(Integer jid);
+	
+	// 單筆查詢
+	Job get(Integer jid);
+	
+	// 單筆查詢-目前所有資料筆數
+	int getCount();
+	
+	// 多筆查詢-不分頁
+	List<Job> query();
+	
+	// 多筆查詢-分頁
+	List<Job> queryPage(int pageNo);
+}
