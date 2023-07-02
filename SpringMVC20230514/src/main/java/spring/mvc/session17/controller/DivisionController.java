b16b141315e34a9e75ccc1ae45124c924d8fb5f9
vincenttuan <vincentjava@yahoo.com.tw>
2023-07-02 15:32:21 +0800

    mvc

--- SpringMVC20230514/src/main/java/spring/mvc/session17/controller/DivisionController.java
+++ SpringMVC20230514/src/main/java/spring/mvc/session17/controller/DivisionController.java
@@ -3,6 +3,7 @@ package spring.mvc.session17.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import spring.mvc.session17.entity.Division;
@@ -13,6 +14,13 @@ public class DivisionController {
 	
 	@GetMapping
 	public String index(@ModelAttribute Division division) {
+		return "session17/division";
+	}
+	
+	@PostMapping
+	public String operation(@ModelAttribute Division division) {
+		int result = division.getX() / division.getY();
+		division.setResult(result);
 		return "session17/division";
 	}
 	
