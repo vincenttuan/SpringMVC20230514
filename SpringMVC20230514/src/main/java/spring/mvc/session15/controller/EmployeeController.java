3b7f871cbb22f98374015c7927af0256295067ba
vincenttuan <vincentjava@yahoo.com.tw>
2023-07-02 13:11:05 +0800

    jdbc

---
+++ SpringMVC20230514/src/main/java/spring/mvc/session15/controller/EmployeeController.java
@@ -0,0 +1,55 @@
+package spring.mvc.session15.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import spring.mvc.session15.entity.Employee;
+import spring.mvc.session15.repository.EmployeeDao;
+import spring.mvc.session15.repository.JobDao;
+
+@Controller
+@RequestMapping("/session15/employee")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeDao employeeDao;
+	
+	@Autowired
+	private JobDao jobDao;
+	
+	@GetMapping("/")
+	public String index(@ModelAttribute Employee employee, Model model, HttpSession session) {
+		model.addAttribute("_method", "POST");
+		setBaseModelAttribute(model, session);
+		return "session15/employee";
+	}
+	
+	// 取得總頁數
+	private int getPageCount() {
+		int count = employeeDao.getCount();
+		int limit = EmployeeDao.LIMIT;
+		int pageCount = (int)(Math.ceil((double)count/limit));
+		return pageCount;
+	}
+		
+	// 設定 base model attr value
+	private void setBaseModelAttribute(Model model, HttpSession session) {
+		// 取得目前使用者所在的分頁
+		String sessionNum = session.getAttribute("num") + "";
+		if(sessionNum.length() > 0 && !sessionNum.equals("null")) {
+			int num = Integer.parseInt(sessionNum);
+			model.addAttribute("employees", employeeDao.queryPage(num));
+		} else {
+			model.addAttribute("employees", employeeDao.query());
+		}
+		model.addAttribute("jobs", jobDao.query());
+		model.addAttribute("pageCount", getPageCount());
+	}
+	
+}
