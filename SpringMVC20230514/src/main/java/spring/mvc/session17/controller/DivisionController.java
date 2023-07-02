f06b87b8a6ff5775dd095a62f63fa869b2234915
vincenttuan <vincentjava@yahoo.com.tw>
2023-07-02 16:25:09 +0800

    mvc

--- SpringMVC20230514/src/main/java/spring/mvc/session17/controller/DivisionController.java
+++ SpringMVC20230514/src/main/java/spring/mvc/session17/controller/DivisionController.java
@@ -32,7 +32,7 @@ public class DivisionController {
 	// 捕獲使用者輸入資料格式不正確的例外：BindException
 	// 數學錯誤例外：ArithmeticException
 	//@ExceptionHandler({BindException.class, ArithmeticException.class})
-	@ExceptionHandler({ArithmeticException.class})
+	@ExceptionHandler({BindException.class})
 	public String catchException(Exception ex, Model model, HttpServletRequest request) {
 		// 那一頁發生問題 ?
 		String referer = request.getHeader("Referer");
