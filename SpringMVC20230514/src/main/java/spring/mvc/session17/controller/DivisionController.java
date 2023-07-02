da332159c8569e216ec5cab64d9f46da4ff6b90b
vincenttuan <vincentjava@yahoo.com.tw>
2023-07-02 15:25:31 +0800

    mvc

---
+++ SpringMVC20230514/src/main/java/spring/mvc/session17/controller/DivisionController.java
@@ -0,0 +1,14 @@
+package spring.mvc.session17.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/session17/division")
+public class DivisionController {
+	
+	
+	
+}
