34b6f1a036e6735a9ea51869543a5c255a33c6da
vincenttuan <vincentjava@yahoo.com.tw>
2023-07-09 14:00:48 +0800

    spring

---
+++ SpringMVC20230514/src/main/java/spring/mvc/session17/controller/ProductAndMasterController.java
@@ -0,0 +1,41 @@
+package spring.mvc.session17.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import spring.mvc.session17.entity.Master;
+import spring.mvc.session17.entity.Product;
+
+@Controller
+@RequestMapping("/session17/product_master")
+public class ProductAndMasterController {
+	private List<Product> products = new ArrayList<>();
+	private List<Master> masters = new ArrayList<>();
+	
+	
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("products", products);
+		model.addAttribute("masters", masters);
+		return "session17/product_master";
+	}
+	
+	@PostMapping("/")
+	public String add(@ModelAttribute("a") Product product, @ModelAttribute("b") Master master) {
+		products.add(product);
+		masters.add(master);
+		
+		return "redirect:./";
+	}
+	
+}
+ 
---
+++ SpringMVC20230514/src/main/java/spring/mvc/session17/entity/Product.java
@@ -0,0 +1,25 @@
+package spring.mvc.session17.entity;
+
+public class Product {
+	private String name; // 品名
+	private Integer price; // 價格
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", price=" + price + "]";
+	}
+	
+	
+}
