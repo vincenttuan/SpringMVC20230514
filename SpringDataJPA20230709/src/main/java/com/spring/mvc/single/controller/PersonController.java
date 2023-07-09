39c9ab9623a9a6d6fa88046fd49728bf7e35db1a
vincenttuan <vincentjava@yahoo.com.tw>
2023-07-09 16:23:17 +0800

    spring

--- SpringDataJPA20230709/src/main/java/com/spring/mvc/single/controller/PersonController.java
+++ SpringDataJPA20230709/src/main/java/com/spring/mvc/single/controller/PersonController.java
@@ -14,7 +14,7 @@ import com.spring.mvc.single.entity.Person;
 import com.spring.mvc.single.repository.PersonRepository;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/person")
 public class PersonController {
 	
 	@Autowired
