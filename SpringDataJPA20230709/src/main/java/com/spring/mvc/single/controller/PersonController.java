5b53e0c9fd0ffd4b772766130f77af096bf71388
vincenttuan <vincentjava@yahoo.com.tw>
2023-07-16 13:57:48 +0800

    jpa

--- SpringDataJPA20230709/src/main/java/com/spring/mvc/single/controller/PersonController.java
+++ SpringDataJPA20230709/src/main/java/com/spring/mvc/single/controller/PersonController.java
@@ -3,6 +3,9 @@ package com.spring.mvc.single.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.spring.mvc.single.entity.Person;
 import com.spring.mvc.single.repository.PersonRepository;
@@ -61,6 +65,26 @@ public class PersonController {
 	public String deleteById(@PathVariable long id) {
 		personRepository.delete(id);
 		return "redirect:/mvc/person/";
+	}
+	
+	// 查詢分頁
+	// 範例路徑：/page, /page?no=1, /page?no=5 etc...
+	@GetMapping("/page")
+	public String page(@RequestParam(name = "no", required = false, defaultValue = "0") Integer no, Model model) {
+		int pageNo = no;
+		int pageSize = 5;
+		// 排序
+		Sort.Order order = new Sort.Order(Sort.Direction.ASC, "id");
+		Sort sort = new Sort(order);
+		// 分頁
+		PageRequest pageRequest = new PageRequest(pageNo, pageSize, sort);
+		Page<Person> page = personRepository.findAll(pageRequest);
+		List<Person> persons = page.getContent(); 
+		model.addAttribute("persons", persons);
+		model.addAttribute("pageNo", pageNo);
+		model.addAttribute("totalPage", page.getTotalPages());
+		
+		return "person/page";
 	}
 	
 }
